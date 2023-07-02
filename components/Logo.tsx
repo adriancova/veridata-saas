4625dccbce4fd487687ef9efb7ee17f13f2ebd92
Adrian Covarrubias <adriancova@outlook.com>
2023-07-02 12:57:55 +0100

    py applying breaking point in navbar

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <a class='py-4 my-navbar flex flex-row items-center' href='/#'>
+    <a class='my-navbar flex flex-row items-center py-3 md:py-6' href='/#'>
       <img class='h-12 mr-4 md:h-16' src='logo.svg' alt='Logo' />
       <h1 class='text-white text-lg md:text-2xl'>Veridata</h1>
     </a>
