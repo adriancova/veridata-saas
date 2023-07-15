4d814028704ec485649373708a1b2f10ed521af5
Adrian Covarrubias <adriancova@outlook.com>
2023-07-15 10:22:47 -0700

    latest fresh

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,57 +2,63 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from './routes/_404.tsx';
-import * as $1 from './routes/_500.tsx';
-import * as $2 from './routes/_app.tsx';
-import * as $3 from './routes/_middleware.ts';
-import * as $4 from './routes/account/_middleware.ts';
-import * as $5 from './routes/account/index.tsx';
-import * as $6 from './routes/account/manage.ts';
-import * as $7 from './routes/account/upgrade.ts';
-import * as $8 from './routes/api/stripe-webhooks.ts';
-import * as $9 from './routes/api/vote.ts';
-import * as $10 from './routes/blog/[slug].tsx';
-import * as $11 from './routes/blog/index.tsx';
-import * as $12 from './routes/callback.ts';
-import * as $13 from './routes/index.tsx';
-import * as $14 from './routes/item/[id].tsx';
-import * as $15 from './routes/pricing.tsx';
-import * as $16 from './routes/signin.ts';
-import * as $17 from './routes/signout.ts';
-import * as $18 from './routes/stats.tsx';
-import * as $19 from './routes/submit.tsx';
-import * as $20 from './routes/user/[username].tsx';
-import * as $$0 from './islands/NavBar.tsx';
-import * as $$1 from './islands/VoteButton.tsx';
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_500.tsx";
+import * as $2 from "./routes/_app.tsx";
+import * as $3 from "./routes/_middleware.ts";
+import * as $4 from "./routes/account/_middleware.ts";
+import * as $5 from "./routes/account/index.tsx";
+import * as $6 from "./routes/account/manage.ts";
+import * as $7 from "./routes/account/notifications.tsx";
+import * as $8 from "./routes/account/upgrade.ts";
+import * as $9 from "./routes/api/stripe-webhooks.ts";
+import * as $10 from "./routes/api/vote.ts";
+import * as $11 from "./routes/blog/[slug].tsx";
+import * as $12 from "./routes/blog/index.tsx";
+import * as $13 from "./routes/callback.ts";
+import * as $14 from "./routes/index.tsx";
+import * as $15 from "./routes/item/[id].tsx";
+import * as $16 from "./routes/pricing.tsx";
+import * as $17 from "./routes/signin.ts";
+import * as $18 from "./routes/signout.ts";
+import * as $19 from "./routes/stats.tsx";
+import * as $20 from "./routes/submit.tsx";
+import * as $21 from "./routes/user/[username].tsx";
+import * as $$0 from "./islands/Chart.tsx";
+import * as $$1 from "./islands/NavBar.tsx";
+import * as $$2 from "./islands/PageInput.tsx";
+import * as $$3 from "./islands/VoteButton.tsx";
 
 const manifest = {
   routes: {
-    './routes/_404.tsx': $0,
-    './routes/_500.tsx': $1,
-    './routes/_app.tsx': $2,
-    './routes/_middleware.ts': $3,
-    './routes/account/_middleware.ts': $4,
-    './routes/account/index.tsx': $5,
-    './routes/account/manage.ts': $6,
-    './routes/account/upgrade.ts': $7,
-    './routes/api/stripe-webhooks.ts': $8,
-    './routes/api/vote.ts': $9,
-    './routes/blog/[slug].tsx': $10,
-    './routes/blog/index.tsx': $11,
-    './routes/callback.ts': $12,
-    './routes/index.tsx': $13,
-    './routes/item/[id].tsx': $14,
-    './routes/pricing.tsx': $15,
-    './routes/signin.ts': $16,
-    './routes/signout.ts': $17,
-    './routes/stats.tsx': $18,
-    './routes/submit.tsx': $19,
-    './routes/user/[username].tsx': $20,
+    "./routes/_404.tsx": $0,
+    "./routes/_500.tsx": $1,
+    "./routes/_app.tsx": $2,
+    "./routes/_middleware.ts": $3,
+    "./routes/account/_middleware.ts": $4,
+    "./routes/account/index.tsx": $5,
+    "./routes/account/manage.ts": $6,
+    "./routes/account/notifications.tsx": $7,
+    "./routes/account/upgrade.ts": $8,
+    "./routes/api/stripe-webhooks.ts": $9,
+    "./routes/api/vote.ts": $10,
+    "./routes/blog/[slug].tsx": $11,
+    "./routes/blog/index.tsx": $12,
+    "./routes/callback.ts": $13,
+    "./routes/index.tsx": $14,
+    "./routes/item/[id].tsx": $15,
+    "./routes/pricing.tsx": $16,
+    "./routes/signin.ts": $17,
+    "./routes/signout.ts": $18,
+    "./routes/stats.tsx": $19,
+    "./routes/submit.tsx": $20,
+    "./routes/user/[username].tsx": $21,
   },
   islands: {
-    './islands/NavBar.tsx': $$0,
-    './islands/VoteButton.tsx': $$1,
+    "./islands/Chart.tsx": $$0,
+    "./islands/NavBar.tsx": $$1,
+    "./islands/PageInput.tsx": $$2,
+    "./islands/VoteButton.tsx": $$3,
   },
   baseUrl: import.meta.url,
 };
