1c80bb1590f217c216d6f2a2c72ef2af09b6f5d1
Adrian Covarrubias <adriancova.95@gmail.com>
2023-08-08 11:36:55 -0700

    javascript formatter

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,26 @@
 (function () {
-  'use strict';
+  "use strict";
 
   // ======= Sticky
   window.onscroll = function () {
-    const ud_header = document.querySelector('.ud-header');
+    const ud_header = document.querySelector(".ud-header");
     const sticky = ud_header.offsetTop;
-    const logo = document.querySelector('.my-navbar img');
-    const title = document.querySelector('.my-navbar h1');
+    const logo = document.querySelector(".my-navbar img");
+    const title = document.querySelector(".my-navbar h1");
 
     if (window.pageYOffset > sticky) {
-      ud_header.classList.add('sticky');
+      ud_header.classList.add("sticky");
     } else {
-      ud_header.classList.remove('sticky');
+      ud_header.classList.remove("sticky");
     }
 
     // === logo change
-    if (ud_header.classList.contains('sticky')) {
-      logo.src = 'images/logo/logo2.webp';
-      title.classList.remove('text-white');
+    if (ud_header.classList.contains("sticky")) {
+      logo.src = "images/logo/logo2.webp";
+      title.classList.remove("text-white");
     } else {
-      logo.src = 'images/logo/logo.svg';
-      title.classList.add('text-white');
+      logo.src = "images/logo/logo.svg";
+      title.classList.add("text-white");
     }
   };
 
