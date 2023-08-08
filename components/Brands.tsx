6d15c15d9c35ca497b4bdf5ed1ff1928497a568a
Adrian Covarrubias <adriancova.95@gmail.com>
2023-08-08 12:50:13 -0700

    Brands with tooltip and no js needed

---
+++ components/Brands.tsx
@@ -0,0 +1,42 @@
+import { ComponentChildren } from "preact";
+
+const Tooltip = (
+  { text, children }: { text: string; children: ComponentChildren },
+) => {
+  return (
+    <div class="relative inline-block group">
+      {children}
+      <div class="absolute hidden group-hover:inline-block bg-black text-white text-center p-1 rounded-md z-10 w-32 transform -translate-x-1/2">
+        {text}
+      </div>
+    </div>
+  );
+};
+
+const Brands = () => {
+  return (
+    <div
+      class="ud-hero-brands-wrapper wow fadeInUp flex flex-row justify-center items-center gap-6 pb-6"
+      data-wow-delay=".3s"
+    >
+      <Tooltip text="API powered by Deno">
+        <img class="h-14" src="/images/brands/deno.svg" alt="deno" />
+      </Tooltip>
+      <Tooltip text="Website built with Fresh">
+        <img class="h-10" src="/images/brands/fresh.svg" alt="fresh" />
+      </Tooltip>
+      <Tooltip text="Styled with Tailwind">
+        <img
+          class="h-10"
+          src="/images/brands/tailwind.svg"
+          alt="tailwind"
+        />
+      </Tooltip>
+      <Tooltip text="Standard Open API v3">
+        <img class="h-16" src="/images/brands/openapi.svg" alt="openapi" />
+      </Tooltip>
+    </div>
+  );
+};
+
+export default Brands;
