b0c92e4d9f9acae93a6d54c5e415296f3d77fbd9
Adrian Covarrubias <adriancova.95@gmail.com>
2023-08-13 15:05:32 -0700

    brands tooltip in spanish

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -19,20 +19,20 @@ const Brands = () => {
       class="ud-hero-brands-wrapper wow fadeInUp flex flex-row justify-center items-center gap-6 pb-6"
       data-wow-delay=".3s"
     >
-      <Tooltip text="API powered by Deno">
+      <Tooltip text="API desarrollada con Deno">
         <img class="h-14" src="/images/brands/deno.svg" alt="deno" />
       </Tooltip>
-      <Tooltip text="Website built with Fresh">
+      <Tooltip text="Website construido con Fresh">
         <img class="h-10" src="/images/brands/fresh.svg" alt="fresh" />
       </Tooltip>
-      <Tooltip text="Styled with Tailwind">
+      <Tooltip text="Estilos con Tailwind">
         <img
           class="h-10"
           src="/images/brands/tailwind.svg"
           alt="tailwind"
         />
       </Tooltip>
-      <Tooltip text="Standard Open API v3">
+      <Tooltip text="Estándar Open API v3">
         <img class="h-16" src="/images/brands/openapi.svg" alt="openapi" />
       </Tooltip>
     </div>
