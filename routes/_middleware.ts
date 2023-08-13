62989ddd617fcb68904a67ce54823b12a7e2c787
Adrian Covarrubias <adriancova.95@gmail.com>
2023-08-13 15:07:04 -0700

    removing redirect

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,8 +1,6 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { getSessionId } from "kv_oauth";
-import { redirect } from "@/utils/redirect.ts";
-import { Status } from "std/http/http_status.ts";
 import { getUserBySession, ifUserHasNotifications } from "@/utils/db.ts";
 import { incrVisitsCountByDay } from "@/utils/db.ts";
 import type { MetaProps } from "@/components/Meta.tsx";
@@ -10,16 +8,6 @@ import type { MetaProps } from "@/components/Meta.tsx";
 export interface State extends MetaProps {
   sessionId?: string;
   hasNotifications?: boolean;
-}
-
-async function redirectToNewOrigin(
-  req: Request,
-  ctx: MiddlewareHandlerContext,
-) {
-  const { hostname } = new URL(req.url);
-  return hostname === "saaskit.deno.dev"
-    ? redirect("https://hunt.deno.land", Status.Found)
-    : await ctx.next();
 }
 
 async function setState(req: Request, ctx: MiddlewareHandlerContext<State>) {
@@ -47,7 +35,6 @@ async function recordVisit(
 }
 
 export const handler = [
-  redirectToNewOrigin,
   setState,
   recordVisit,
 ];
